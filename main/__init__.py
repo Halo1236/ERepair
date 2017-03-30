698b39168b623850a5dd370c1d97ea086e3e8928
Halo <wuyihuangw@gmail.com>
2017-03-30 21:34:52 +0800

    v1.0.0.1

--- main/__init__.py
+++ main/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
 import logging
 from logging.handlers import RotatingFileHandler
--- main/routes.py
+++ main/routes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
 from flask import render_template, request, abort, jsonify, session, redirect, url_for
 
